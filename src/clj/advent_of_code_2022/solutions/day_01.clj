6132870c3054797ca0da01ee7adaa07e7e265ed7
Tony Bradley <antbradley91@gmail.com>
2022-12-01 05:42:28 -0500

    day 1

--- src/clj/advent_of_code_2022/solutions/day_01.clj
+++ src/clj/advent_of_code_2022/solutions/day_01.clj
@@ -1,12 +1,22 @@
-(ns advent-of-code-2022.solutions.day-01)
+(ns advent-of-code-2022.solutions.day-01
+  (:require [clojure.string :as str]))
 
-(defn part-1
-  [input]
-  input)
+(defn parse-input [input]
+  (->> (str/split input #"\n\n")
+       (map (comp #(map read-string %) #(str/split % #"\n")))))
 
-(defn part-2
-  [input]
-  input)
+(defn part-1 [input]
+  (->> (parse-input input)
+       (map #(reduce + 0 %))
+       (#(apply max %))))
+
+(defn part-2 [input]
+  (->> (parse-input input)
+       (map #(reduce + 0 %))
+       sort
+       reverse
+       (take 3)
+       (reduce + 0)))
 
 (def output
   {"Part One" (-> (slurp "resources/input/day_01.txt") part-1)
--- test/clj/advent_of_code_2022/day_01_test.clj
+++ test/clj/advent_of_code_2022/day_01_test.clj
@@ -3,10 +3,30 @@
    [clojure.test :as t]
    [advent-of-code-2022.solutions.day-01 :as day-01]))
 
-(t/deftest day-01-test
-  (t/testing "Part 1 - Sample"
-    (let [input "1234"]
-      (t/is (= "FIX ME" (day-01/part-1 input)))))
-  (t/testing "Part 1"
+(def sample-input "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000")
+
+(t/deftest day-01-part-01-test
+  (t/testing "Sample"
+    (let [input sample-input]
+      (t/is (= 24000 (day-01/part-1 input)))))
+  (t/testing "Actual"
     (let [input (slurp "resources/input/day_01.txt")]
-      (t/is (= "FIX ME" (day-01/part-1 input))))))
+      (t/is (= 66719 (day-01/part-1 input))))))
+
+(t/deftest day-01-part-02-test
+  (t/testing "Sample"
+    (let [input sample-input]
+      (t/is (= 45000 (day-01/part-2 input))))))
