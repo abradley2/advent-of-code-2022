f1bac49d744a349bc3f159708adc0eca9d96665b
tony <antbradley91@gmail.com>
2022-12-22 16:00:23 -0500

    finally done parsing

--- src/clj/advent_of_code_2022/solutions/day_11.clj
+++ src/clj/advent_of_code_2022/solutions/day_11.clj
@@ -1,8 +1,37 @@
 (ns advent-of-code-2022.solutions.day-11
   (:require
+   [clojure.string :as str]
    [clojure.spec.alpha :as s]))
 
-(defn parse-test [raw-test] raw-test)
+(defn parse-condition [monkey-id raw-cond]
+  (let [[compare, effect] (.split raw-cond ":")]
+    {:compare #(= % (read-string (-> (re-matches #"\s*If\s+(.+)" (.trim compare)) (get 1))))
+     :effect (or
+              (some-> (re-matches #"throw\s+to\s+monkey\s+(\d+)" (.trim effect))
+                      (get 1)
+                      ((fn [to-id] (fn [item all-monkies]
+                          (map
+                           (fn [monkey]
+                             (cond
+                               (= (:name monkey) monkey-id)
+                               (assoc monkey :items (filter #(= item %) (:items monkey)))
+                               (= (:name monkey) to-id)
+                               (update monkey :items #(concat item (cons % nil)))
+                               :else monkey))
+                           all-monkies)))))
+              effect)}))
+
+(defn parse-test [monkey-id raw-test]
+  (let [lines (.split raw-test "\n")
+        test-stmt (first lines)
+        cond-stmts (rest lines)]
+    {:check (s/assert some? (some
+                             identity
+                             [(some-> (re-matches #"\s*divisible\s+by\s+(\d+).*" test-stmt)
+                                      (get 1)
+                                      read-string
+                                      ((fn [v] #(= 0 (mod v %)))))]))
+     :conditions (map (partial parse-condition monkey-id) cond-stmts)}))
 
 (defn run-operation [old op]
   ((:fn op)
@@ -15,24 +44,22 @@
      :fn (read-string (s/assert some? (get matches 2)))
      :arg-2 (->> (get matches 3) (s/assert some?) (#(if (= % "old") :old (read-string %))))}))
 
-(defn parse-trait [raw-trait]
-  (as-> (.split raw-trait ":") [name body]
+(defn parse-trait [monkey-id raw-trait]
+  (let [[name body] (str/split raw-trait #":" 2) ]
     (cond
       (= name "Starting items") {:items (read-string (str "(" body ")"))}
       (= name "Operation") {:operation (parse-operation body)}
-      (= name "Test") {:test (parse-test body)})))
+      (= name "Test") {:test (parse-test monkey-id body)})))
 
 (defn parse-monkey [raw-monkey]
   (let [lines (.split raw-monkey (str #"\n\s{2}(?=\w)"))
-        name (first lines)
-        traits (->> (map parse-trait (rest lines)) (reduce merge {}))]
-
-    {:name name :traits traits}))
+        id (re-find #"\d+" (first lines))
+        traits (->> (map (partial parse-trait id) (rest lines)) (reduce merge {}))]
+    (merge {:id id} traits)))
 
 (defn parse-input [input]
   (->> (.split (s/assert string? input) (str #"\n\n"))
        (map parse-monkey)))
-
 
 (defn part-1
   [_input]
