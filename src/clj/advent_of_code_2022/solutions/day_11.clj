e8eceb7eb3d14415f16bab7e3cae00374eab7a3f
Tony Bradley <antbradley91@gmail.com>
2022-12-22 10:12:55 -0500

    start day 11

--- src/clj/advent_of_code_2022/solutions/day_11.clj
+++ src/clj/advent_of_code_2022/solutions/day_11.clj
@@ -1,4 +1,24 @@
-(ns advent-of-code-2022.solutions.day-11)
+(ns advent-of-code-2022.solutions.day-11
+  (:require
+   [clojure.spec.alpha :as s]))
+
+(defn parse-trait [input] input)
+
+(defmacro make-op [_new _eq argA op argB] `((fn [old] (~op ~argA ~argB))))
+
+
+
+(defn parse-monkey [input]
+  (let [lines (.split input (str #"\n\s{2}(?=\w)"))
+        name (first lines)
+        traits (map parse-trait (rest lines))]
+
+    {:name name :traits traits}))
+
+(defn parse-input [input]
+  (->> (.split (s/assert string? input) (str #"\n\n"))
+       (map parse-monkey)))
+
 
 (defn part-1
   [_input]
