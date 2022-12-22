d7a22e1be8a2cea6d4b833a1e2a98d09998002be
Tony Bradley <antbradley91@gmail.com>
2022-12-22 05:34:42 -0500

    not implemented

--- src/clj/advent_of_code_2022/solutions/day_14.clj
+++ src/clj/advent_of_code_2022/solutions/day_14.clj
@@ -1,12 +1,12 @@
 (ns advent-of-code-2022.solutions.day-14)
 
 (defn part-1
-  [input]
-  input)
+  [_input]
+  "Not implemented")
 
 (defn part-2
-  [input]
-  input)
+  [_input]
+  "Not implemented")
 
 (def output
   {"Part One" (-> (slurp "resources/input/day_14.txt") part-1)
--- src/clj/advent_of_code_2022/solutions/day_15.clj
+++ src/clj/advent_of_code_2022/solutions/day_15.clj
@@ -1,12 +1,12 @@
 (ns advent-of-code-2022.solutions.day-15)
 
 (defn part-1
-  [input]
-  input)
+  [_input]
+  "Not implemented")
 
 (defn part-2
-  [input]
-  input)
+  [_input]
+  "Not implemented")
 
 (def output
   {"Part One" (-> (slurp "resources/input/day_15.txt") part-1)
